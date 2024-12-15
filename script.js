39e1ce9381e3a52d78d55c4ac7ce15ea8a83df2e
Luis Tanese <luistanese@gmail.com>
2024-12-14 22:01:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const toggle = () => {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".phone-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+};
